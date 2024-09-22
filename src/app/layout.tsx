cf347a6916012fb4f901b5461a42ba7576022721
Nopnapat Norasri <nopnapatn@gmail.com>
2024-09-23 00:38:40 +0700

    ci(*): change new structure & update version package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,21 +3,21 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/cn"
 import "@/styles/globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Nopnapatn",
-  description: "Personal blog by Nopnapatn"
+  title: "Nopnapat Norasri",
+  description: "Nopnapat Norasri's personal website"
 }
 
 export default function RootLayout({
   children
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { TimelineDemo } from "@/components/common/timeline"
-import Footer from "@/components/layout/footer"
-import { Card } from "@/components/ui/card"
-
-import Header from "../components/layout/header"
-
 export default function Home() {
-  return (
-    <>
-      <div className="mx-auto max-w-2xl px-4 md:px-0 md:pt-8">
-        <Header />
-        <Card className="bg-background/95 p-8 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-          <TimelineDemo />
-        </Card>
-        <Footer />
-      </div>
-    </>
-  )
+  return <>Home</>
 }
