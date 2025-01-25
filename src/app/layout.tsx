826fe7b79ab92d3f0af8faf94a8817b96d1d2e66
Nopnapat Norasri <nopnapatn@gmail.com>
2025-01-25 18:03:04 +0700

    build(*): add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next"
 
 import Footer from "@/components/layout/footer"
@@ -37,6 +38,7 @@ export default function RootLayout({
             <Footer />
           </SiteProvider>
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
