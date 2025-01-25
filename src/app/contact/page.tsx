9da55a4b526d414ef8c91f1e1c6dcba3714f6ef3
Nopnapat Norasri <nopnapatn@gmail.com>
2025-01-25 19:52:06 +0700

    feat(*): make it more feel like smooth

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,17 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+import { cn } from "@/lib/utils"
+
 export default function Page() {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className={cn("overflow-hidden rounded-3xl")}
+    >
       <p>Comming Soon..</p>
-    </div>
+    </motion.div>
   )
 }
